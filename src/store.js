cc5e4c8e267c207ed05b0d4c358a2745b3c80eaf
t2t2 <taavot@gmail.com>
2019-03-08 00:07:33 +0200

    Add preload for spreadsheet to give slight speedup

--- src/store.js
+++ src/store.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 
 import dataTransformer from './data-transform'
 
+const client = axios.create()
+delete client.defaults.headers.common['Accept'] // For preload accuracy
+
 export const state = Vue.observable({
 	loading: true,
 	error: null,
@@ -28,13 +31,40 @@ export const state = Vue.observable({
 
 export async function fetchData(spreadsheetUrl) {
 	try {
-		const {data} = await axios.get(spreadsheetUrl, {
-			responseType: 'document'
-		})
+		const {data} = await fetchWithCORSRetry(spreadsheetUrl)
 
 		state.draft = dataTransformer(data)
 	} catch (err) {
+		console.error('error', err)
 		state.error = err
 	}
 	state.loading = false
+}
+
+/**
+ * If user has previously loaded spreadsheet, the cached response might throw a network error due to cors mismatch.
+ * So add '?cors=1' as unique (it actually does nothing, google server responds the same), and if that is
+ * busted (localhost and draft.diamondclub.tv clash) add timestamp
+ *
+ * @param {string} spreadsheetUrl
+ */
+async function fetchWithCORSRetry(spreadsheetUrl) {
+	try {
+		return await axios.get(spreadsheetUrl, {
+			responseType: 'document',
+			params: {
+				cors: 1
+			}
+		})
+	} catch(err) {
+		if (err.message === 'Network Error') {
+			return await axios.get(spreadsheetUrl, {
+				responseType: 'document',
+				params: {
+					cors: Date.now()
+				}
+			})
+		}
+		throw err
+	}
 }
