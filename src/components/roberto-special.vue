2f3545d9a93d7fbba1141c721214b9c962f11ddf
t2t2 <taavot@gmail.com>
2019-07-23 16:33:34 +0300

    Roberto easter egg (#1)

---
+++ src/components/roberto-special.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="message">
+		<div class="message-header is-block">
+			<a href="https://twitter.com/vincent404/status/1151930446898446341" target="_blank" style="text-decoration: none" class="columns is-vcentered">
+				<div class="column">
+					<h1 class="is-size-2 is-size-3-desktop is-size-2-widescreen">
+						#7 CosmicRadioTV
+					</h1>
+				</div>
+				<div class="column has-text-left has-text-right-tablet">
+					<span class="is-size-4 is-size-3-tablet">4,300,000 EEK</span>
+				</div>
+			</a>
+		</div>
+		<div class="message-body">
+			<div>
+				<a href="https://www.imdb.com/title/tt0351940/" style="text-decoration: none">Vanad ja kobedad saavad jalad alla</a>&nbsp;
+				<span class="has-text-grey"><span>4,300,000 EEK</span></span>
+			</div>
+			<div>
+				<a href="https://news.err.ee/945558/christopher-nolan-spy-thriller-to-be-filmed-in-tallinn" style="text-decoration: none">Christopher Nolan's Tenet</a>&nbsp;
+				<span class="has-text-grey">(releases <time>July 17, 2020</time>)</span>
+			</div>
+		</div>
+	</div>
+</template>
